efe93bcb891efb2373fcbf1a8bf160c623b11f5a
Nick Barrett <nick@oxygem.com>
2020-06-11 21:05:22 +0100

    Add `--host` and `--port` arguments.

--- miniwiki/__main__.py
+++ miniwiki/__main__.py
@@ -25,8 +25,10 @@ def import_pages(import_filename):
 @click.command()
 @click.option('import_filename', '--import', type=click.Path(exists=True))
 @click.option('--initdb', is_flag=True, default=False)
+@click.option('--host', default='0.0.0.0')
+@click.option('--port', type=int, default=5000)
 @click.argument('config_filename', type=click.Path(exists=True))
-def start_miniwiki(import_filename, initdb, config_filename):
+def start_miniwiki(import_filename, initdb, host, port, config_filename):
     config = load_config(config_filename)
     app = make_app(config)
 
@@ -40,7 +42,7 @@ def start_miniwiki(import_filename, initdb, config_filename):
         return import_pages(import_filename)
 
     if config['debug']:
-        app.run()
+        app.run(host=host, port=port)
     else:
-        server = wsgi.Server(('0.0.0.0', 5000), app)
+        server = wsgi.Server((host, port), app)
         server.start()

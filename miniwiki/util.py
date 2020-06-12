838643c1a7c568c650646e3cf2c434e1a867e1db
Nick Barrett <nick@oxygem.com>
2020-06-12 12:59:58 +0100

    Fix capitalization function (don't want to lowercase rest).

--- miniwiki/util.py
+++ miniwiki/util.py
@@ -4,9 +4,18 @@ from markdown import Markdown
 from mdx_linkify.mdx_linkify import LinkifyExtension
 
 
+def capitalize_first_letter(string):
+    if string:
+        return string[0].upper() + string[1:]
+    return ''
+
+
 def get_path_and_name(path_and_name):
-    page_path = sep.join([bit.title() for bit in path.dirname(path_and_name).split(sep)])
-    page_name = path.basename(path_and_name).title()
+    page_path = sep.join([
+        capitalize_first_letter(bit)
+        for bit in path.dirname(path_and_name).split(sep)
+    ])
+    page_name = capitalize_first_letter(path.basename(path_and_name))
     return page_path, page_name
 
 
@@ -32,6 +41,6 @@ def split_path_locations(path_name):
             continue
 
         sub_path = f'/{sep.join(path_bits[:i])}'
-        paths.append((sub_path, bit.title()))
+        paths.append((sub_path, capitalize_first_letter(bit)))
 
     return paths

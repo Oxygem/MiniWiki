e4838a73c0db09619b46bb6b1e5380c246b44709
Nick Barrett <nick@oxygem.com>
2020-06-12 11:51:46 +0100

    Move wiki link/index generation into page model class.

--- miniwiki/models.py
+++ miniwiki/models.py
@@ -1,9 +1,14 @@
+import re
+
 from os import path
 
 from flask import render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-from miniwiki.util import add_wiki_indexes, add_wiki_links, markdownify
+from miniwiki.util import get_path_and_name, markdownify
+
+INDEX_REGEX = re.compile(r'\[index\:([\/\ \w]+)\]')
+LINK_REGEX = re.compile(r'\[\[([\/\|\ \w]+)\]\]')
 
 
 db = SQLAlchemy()
@@ -37,8 +42,8 @@ class PageMixin(object):
 
     def render_toc_and_content(self):
         toc, html = markdownify(self.content)
-        html = add_wiki_indexes(Page, html)
-        html = add_wiki_links(Page, html)
+        html = self.add_wiki_indexes(html)
+        html = self.add_wiki_links(html)
         return toc, html
 
     def render_sidebar(self):
@@ -54,6 +59,24 @@ class PageMixin(object):
             description=self.description,
             keywords=self.keywords,
         )
+
+    def add_wiki_indexes(self, html):
+        def make_index(value):
+            page_path = value.group(1)
+            pages = Page.query.filter_by(path=page_path)
+            return render_template('macro/index_links.html', pages=pages)
+
+        return re.sub(INDEX_REGEX, make_index, html)
+
+    def add_wiki_links(self, html):
+        def make_link(value):
+            page_path, page_name = get_path_and_name(value)
+            page = Page.query.get((page_path, page_name))
+            if page:
+                return f'<a href="{page.url}">{page.title}</a>'
+            return f'<a href="{page_path}{page_name}">{page_path}{page_name}</a>'
+
+        return re.sub(LINK_REGEX, make_link, html)
 
 
 class Page(db.Model, PageMixin):
